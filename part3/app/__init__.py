875390c8a560b0ee89cfd7809cfe7f898058c099
q400400200-pixel <q.400400200@gmail.com>
2026-01-20 20:55:44 +0300

    Rename auth namespace import in __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -6,7 +6,7 @@ from flask_restx import Api
 from app.api.v1 import users, places, reviews, amenities
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager #المكتبه الي تتعامل مع JWT TOKENS عشان تفعل النظام 
-from app.api.v1.auth import api as auth_ns
+from app.api.v1.auth import api as auth
 
 bcrypt = Bcrypt()
 jwt = JWTManager() # ربط مع FLASK 
@@ -30,7 +30,7 @@ def create_app(config_class="development"):
     
     # Register namespaces (we'll add these later)
     api.add_namespace(users.api, path='/api/v1/users')
-    api.add_namespace(auth_ns.api, path='/api/v1/auth') # part3 ربط صفحات تسجيل الدخول بالمسار حقها
+    api.add_namespace(auth, path='/api/v1/auth') # part3 ربط صفحات تسجيل الدخول بالمسار حقها
     # api.add_namespace(places.api, path='/api/v1/places')
     # api.add_namespace(reviews.api, path='/api/v1/reviews')
     # api.add_namespace(amenities.api, path='/api/v1/amenities')
