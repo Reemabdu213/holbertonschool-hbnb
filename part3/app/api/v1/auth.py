99d634b7ac418a5e17af82369c2a0f8144b8ab77
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-01-21 19:09:22 +0300

    Update auth.py

--- part3/app/api/v1/auth.py
+++ part3/app/api/v1/auth.py
@@ -1,5 +1,5 @@
 from flask_restx import Namespace, Resource, fields
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt, get_jwt_identity
 from app.services import facade
 
 api = Namespace('auth', description='Authentication operations')
