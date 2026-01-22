07e9459d00aba78a7839f737bd907ec1645219c3
Reemabdu213 <11972@holbertonstudents.com>
2026-01-22 15:44:49 +0300

    Update __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,36 +1,33 @@
-"""
-Flask application initialization
-"""
 from flask import Flask
 from flask_restx import Api
-from app.api.v1 import users, places, reviews, amenities
+from app.api.v1 import users
 from config import DevelopmentConfig
 from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager #المكتبه الي تتعامل مع JWT TOKENS عشان تفعل النظام 
+from flask_jwt_extended import JWTManager
 from app.api.v1.auth import api as auth
+from app.api.v1.protected import api as protected   # ✅ جديد
 
 bcrypt = Bcrypt()
-jwt = JWTManager() # ربط مع FLASK 
+jwt = JWTManager()
 
-def create_app(config_class=DevelopmentConfig): 
-    """Create and configure the Flask application"""
+def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(DevelopmentConfig)
+
+    app.config["JWT_SECRET_KEY"] = "super-secret-key"  # ✅ مهم
+
     bcrypt.init_app(app)
-    jwt.init_app(app) # ربط JWT مع التطبيق 
-    # Initialize Flask-RESTX API
+    jwt.init_app(app)
+
     api = Api(
         app,
         version='1.0',
         title='HBnB API',
         description='HBnB Application API'
     )
-    
-    # Register namespaces (we'll add these later)
+
     api.add_namespace(users.api, path='/api/v1/users')
-    api.add_namespace(auth, path='/api/v1/auth') # part3 ربط صفحات تسجيل الدخول بالمسار حقها
-    # api.add_namespace(places.api, path='/api/v1/places')
-    # api.add_namespace(reviews.api, path='/api/v1/reviews')
-    # api.add_namespace(amenities.api, path='/api/v1/amenities')
-    
+    api.add_namespace(auth, path='/api/v1/auth')
+    api.add_namespace(protected, path='/api/v1')  # ✅ عشان /api/v1/protected
+
     return app
