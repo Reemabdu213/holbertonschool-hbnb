b193f0733833ea0239efd3968e71427d8fa2ad81
Reemabdu213 <11972@holbertonstudents.com>
2026-01-22 16:08:29 +0300

    Update __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_restx import Api
-from app.api.v1 import users
 from config import DevelopmentConfig
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+
+from app.api.v1 import users
 from app.api.v1.auth import api as auth
-from app.api.v1.protected import api as protected   # ✅ جديد
+from app.api.v1.protected import api as protected
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
@@ -14,8 +15,8 @@ def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(DevelopmentConfig)
 
-    app.config["JWT_SECRET_KEY"] = "super-secret-key"  # ✅ مهم
-
+    # flask-jwt-extended يستخدم SECRET_KEY
+    # لازم تكون موجودة في config
     bcrypt.init_app(app)
     jwt.init_app(app)
 
@@ -28,6 +29,6 @@ def create_app(config_class=DevelopmentConfig):
 
     api.add_namespace(users.api, path='/api/v1/users')
     api.add_namespace(auth, path='/api/v1/auth')
-    api.add_namespace(protected, path='/api/v1')  # ✅ عشان /api/v1/protected
+    api.add_namespace(protected, path='/api/v1')
 
     return app
