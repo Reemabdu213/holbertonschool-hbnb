c24edf44a5ca47e3f0c9b3173e198ab7b94115d0
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-01-29 15:22:48 +0300

    Update user.py

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -5,11 +5,11 @@ from .base_model import BaseModel  # Import BaseModel from its module
 class User(BaseModel):
     __tablename__ = 'users'
     #### Task 6
-     first_name = db.Column(db.String(50), nullable=False)
-     last_name = db.Column(db.String(50), nullable=False)
-     email = db.Column(db.String(120), nullable=False, unique=True)
-     password = db.Column(db.String(128), nullable=False)
-     is_admin = db.Column(db.Boolean, default=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(128), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
    # def __init__(self, first_name, last_name, email, password=None, is_admin=False):
     #    self.id = str(uuid.uuid4())
     #    self.first_name = first_name
