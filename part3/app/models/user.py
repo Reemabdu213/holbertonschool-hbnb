cb86bf17353554c2ac9db81e088c92f5a8a444e5
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-01-29 15:12:55 +0300

    Update user.py

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,18 +1,26 @@
+from app import db, bcrypt
 import uuid
-from app.extensions import bcrypt
+from .base_model import BaseModel  # Import BaseModel from its module
 
-class User:
-    def __init__(self, first_name, last_name, email, password=None, is_admin=False):
-        self.id = str(uuid.uuid4())
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.password = None
-        self.is_admin = is_admin
+class User(BaseModel):
+    __tablename__ = 'users'
+    #### Task 6
+     first_name = db.Column(db.String(50), nullable=False)
+     last_name = db.Column(db.String(50), nullable=False)
+     email = db.Column(db.String(120), nullable=False, unique=True)
+     password = db.Column(db.String(128), nullable=False)
+     is_admin = db.Column(db.Boolean, default=False)
+   # def __init__(self, first_name, last_name, email, password=None, is_admin=False):
+    #    self.id = str(uuid.uuid4())
+    #    self.first_name = first_name
+    #    self.last_name = last_name
+    #    self.email = email
+    #    self.password = None
+   #     self.is_admin = is_admin
         
         # Hash password if provided
-        if password:
-            self.hash_password(password)
+     #   if password:
+    #        self.hash_password(password)
     
     def hash_password(self, password):
         """Hashes the password before storing it."""
