6ce04ea78710885b973d69aafe3718dd6a4bee69
Reemabdu213 <11972@holbertonstudents.com>
2026-02-09 23:40:30 +0300

    Add CORS to run.py

--- part3/run.py
+++ part3/run.py
@@ -3,11 +3,12 @@
 Run the Flask application
 """
 from app import create_app
+from flask_cors import CORS
 
 app = create_app()
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 if __name__ == '__main__':
-    # تاسك 4: إنشاء Admin عند التشغيل
     with app.app_context():
         from app.services.facade import facade
         
